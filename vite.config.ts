d050da2e665ae55287cc1c8f70d4657b91bcb6f3
rhectoroc <rhectoroc@gmail.com>
2026-06-11 09:50:48 -0400

    fix1

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '');
-  const target = env.VITE_PROXY_TARGET || 'http://localhost:3001';
+  const target = env.VITE_PROXY_TARGET || 'https://app.oceanconstruction.us';
 
   return {
     plugins: [react()],
